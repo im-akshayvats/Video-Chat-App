eca3ff3f57cf18f470bfa2a0c8c4202ea7f2ec7e
im-akshayvats <akshayvats2000@gmail.com>
2022-03-03 12:53:05 +0530

    Ready for production.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 const server = require("http").createServer(app);
 const cors = require("cors");
+const path = require("path");
 
 const io = require("socket.io")(server, {
 	cors: {
@@ -13,8 +15,10 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-	res.send('Running');
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+app.get('/*', function (req, res) {
+	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 io.on("connection", (socket) => {
